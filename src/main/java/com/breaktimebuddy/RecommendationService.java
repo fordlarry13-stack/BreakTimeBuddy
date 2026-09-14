1723431845ff241adce91c2c3c1b79da5aa965e9
Kapildhami196 <kapildhami196@gmail.com>
2026-09-13 22:59:46 -0400

    feat: add recommendation request model

--- src/main/java/com/breaktimebuddy/RecommendationService.java
+++ src/main/java/com/breaktimebuddy/RecommendationService.java
@@ -4,5 +4,5 @@ import java.util.concurrent.CompletableFuture;
 
 public interface RecommendationService {
 
-    CompletableFuture<String> getRecommendation(String prompt);
+    CompletableFuture<String> getRecommendation(RecommendationRequest request);
 }
