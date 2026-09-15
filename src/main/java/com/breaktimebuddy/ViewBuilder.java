6881189a567570ca597547915d026b5293b9cb2d
Naruyoko <naru519372@gmail.com>
2026-09-15 20:10:02 +0900

    feat: Break recommendation request/dialog system
    Contains many technical debts:
    - Placeholder for recommendation service requests (to be solved with integration)
    - Lack of tests
    - Significant changes would be needed to add multiple dialog types
    - Sharing of `DialogState` between front/backends.

--- src/main/java/com/breaktimebuddy/ViewBuilder.java
+++ src/main/java/com/breaktimebuddy/ViewBuilder.java
@@ -13,13 +13,20 @@ public class ViewBuilder implements Builder<Region> {
   private final Runnable toggleSession;
   private final Runnable saveConfig;
   private final Runnable loadConfig;
+  private final Runnable requestBreakRecommendationNow;
+  private final Runnable acceptBreakRecommendation;
+  private final Runnable rejectBreakRecommendation;
 
   public ViewBuilder(ViewModel model, Runnable toggleSession, Runnable saveConfig,
-      Runnable loadConfig) {
+      Runnable loadConfig, Runnable requestBreakRecommendationNow,
+      Runnable acceptBreakRecommendation, Runnable rejectBreakRecommendation) {
     this.viewModel = model;
     this.toggleSession = toggleSession;
     this.saveConfig = saveConfig;
     this.loadConfig = loadConfig;
+    this.requestBreakRecommendationNow = requestBreakRecommendationNow;
+    this.acceptBreakRecommendation = acceptBreakRecommendation;
+    this.rejectBreakRecommendation = rejectBreakRecommendation;
   }
 
   @Override
@@ -30,6 +37,15 @@ public class ViewBuilder implements Builder<Region> {
     sessionToggleButton.textProperty().bind(viewModel.sessionStatusTextProperty());
     Label sessionsLabel = new Label();
     sessionsLabel.textProperty().bind(viewModel.sessionsProperty().asString("Sessions: %d"));
+    Label breakRecommendationRequestedLabel = new Label();
+    breakRecommendationRequestedLabel.textProperty().bind(Bindings.format(
+        "Pending break recommendation: %s", viewModel.breakRecommendationRequestedProperty()));
+    Button requestBreakRecommendationNowButton = new Button("Request break recommendation now");
+    requestBreakRecommendationNowButton.setOnAction(e -> requestBreakRecommendationNow.run());
+    DialogDisplay dialogDisplay;
+    dialogDisplay = new DialogDisplay(requestBreakRecommendationNow, acceptBreakRecommendation,
+        rejectBreakRecommendation);
+    dialogDisplay.dialogStateProperty().bind(viewModel.dialogStateProperty());
     Label configFeedbackLabel = new Label();
     configFeedbackLabel.textProperty()
         .bind(Bindings.createStringBinding(
@@ -42,7 +58,8 @@ public class ViewBuilder implements Builder<Region> {
     saveConfigButton.setOnAction(e -> saveConfig.run());
     Button loadConfigButton = new Button("Load config");
     loadConfigButton.setOnAction(e -> loadConfig.run());
-    return new VBox(sampleLabel, sessionToggleButton, sessionsLabel, saveConfigButton,
-        loadConfigButton, configFeedbackLabel);
+    return new VBox(sampleLabel, sessionToggleButton, sessionsLabel,
+        breakRecommendationRequestedLabel, requestBreakRecommendationNowButton, dialogDisplay,
+        saveConfigButton, loadConfigButton, configFeedbackLabel);
   }
 }
